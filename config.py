208a3f3756cc00a8b3f9bb7af9231d8358e9967f
Ilya Kamerilov <kamerilov@gmail.com>
2023-11-20 19:16:05 +0200

    added webdriver_manager

--- config.py
+++ config.py
@@ -1,11 +1,24 @@
 import os
 from dotenv import load_dotenv
-from pydantic import BaseSettings, SecretStr
-
+# from pydantic import BaseSettings, SecretStr
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
 load_dotenv()
 
 
 class AllSettings(BaseSettings):
+    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
     my_chat_id: SecretStr = os.getenv('MY_CHAT_ID', None)
     tg_channels: list = os.getenv('TG_CHANNELS', None)
     bot_token: SecretStr = os.getenv('BOT_TOKEN', None)
+
+
+# class AllSettings(BaseSettings):
+#     my_chat_id: SecretStr = os.getenv('MY_CHAT_ID', None)
+#     tg_channels: list = os.getenv('TG_CHANNELS', None)
+#     bot_token: SecretStr = os.getenv('BOT_TOKEN', None)
+
+
+# settings = AllSettings()
+
+# print(settings.bot_token)
